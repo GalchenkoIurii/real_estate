6d17a38eee626573fb56de0b86538e5d45889a40
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-10-01 09:30:43 +0300

    Added list of items and selected item

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const houses = [
-    {title: 'First house', shortDescription: 'This is short description of First house', price: '50000', image: './images/home_01.jpg'},
-    {title: 'Second house', shortDescription: 'This is short description of Second house', price: '70000', image: '/images/home_02.jpg'},
+    {title: 'First house', shortDescription: 'This is short description of First house', price: '50000', image: 'images/home_01.jpg'},
+    {title: 'Second house', shortDescription: 'This is short description of Second house', price: '70000', image: 'images/home_02.jpg'},
     {title: 'Third house', shortDescription: 'This is short description of Third house', price: '80000', image: 'images/home_03.jpg'},
     {title: 'Fourth house', shortDescription: 'This is short description of Fourth house', price: '55000', image: 'images/home_04.jpg'},
     {title: 'Fifth house', shortDescription: 'This is short description of Fifth house', price: '30000', image: 'images/home_05.jpg'},
@@ -10,6 +10,14 @@ const houses = [
 new Vue({
     el: '#main',
     data: {
-        houses: houses
+        houses: houses,
+        house: houses[0],
+        selectedItem: 0
+    },
+    methods: {
+        selectHouse: function(index) {
+            this.house = houses[index];
+            this.selectedItem = index;
+        }
     }
 })
